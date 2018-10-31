be2c1117a7622b9ecc44cb5702e1fff5c7ae982c
Ali Kadar <a.h.kadar@gmail.com>
2018-11-01 00:38:10 +0100

    minor updates

--- main.cpp
+++ main.cpp
@@ -15,9 +15,11 @@
 
 #include"stopwatch.h"
 
+const double xOrigin(-1), yOrigin(-0.5), zOrigin(-0.5), delta(0.01);
+
 void case1(unsigned nx, unsigned ny, unsigned nz, unsigned NUM_THREADS) {
 
-   // Allocate dynamic memory of type double and size nx*ny*nz
+	// Allocate dynamic memory of type double and size nx*ny*nz
 	double* gridData = new double[nx * ny * nz]();
 
 #pragma omp parallel default(none) num_threads(NUM_THREADS)\
@@ -25,19 +27,25 @@ shared(nx, ny, nz, gridData, std::cout)
 	{
 		unsigned id = omp_get_thread_num(); // This is a run-time library routine.
 		unsigned total = omp_get_num_threads();
-		if (id==0) printf("Total threads executing the parallel region = %d\n", total);
+		//if (id==0) printf("Total threads executing the parallel region = %d\n", total);
 
-#pragma omp for schedule(dynamic,1)
+#pragma omp for schedule(static)
 		for (unsigned kk = 0; kk < nz; kk++)
 			for (unsigned jj = 0; jj < ny; jj++)
 				for (unsigned ii = 0; ii < nx; ii++) {
+
 					unsigned long int idx = ii + nx * jj + nx * ny * kk;
+
+					double xx = xOrigin+ii*delta;
+					double yy = yOrigin+jj*delta;
+					double zz = zOrigin+kk*delta;
+					double dist2 = xx*xx+yy*yy+zz*zz;
 
 					double value = 0;
 
 					unsigned nStencil = 10;
-					for (unsigned itr = 0; itr < nStencil; itr++) {
-						value += exp(-1);
+					for (unsigned itr = 1; itr <= nStencil; itr++) {
+						value += exp(-dist2*itr*itr);
 					}
 
 					gridData[idx] = value;
@@ -54,12 +62,9 @@ int main() {
 	mainWatch.start();
 
 	watch.start();
-	case1(1000,1000,100,1);
-	watch.stop();
-	watch.showTime("case1");
-
-	watch.start();
-	case1(1000,1000,100,1);
+	for (unsigned itr = 1; itr <= 100; itr++) {
+		case1(201, 101, 101, 1);
+	}
 	watch.stop();
 	watch.showTime("case1");
 
